91d651f3d019a5110b979c6693149573673e267b
sagan5 <43873645+sagan5@users.noreply.github.com>
2018-10-18 14:12:26 +0300

    start projektas-vcs-wp

--- partials/banner.php
+++ partials/banner.php
@@ -1,9 +1,8 @@
 <section class="banner">
 	<?php 
 		$image = get_field('hb_banner_image');
-		if( !empty($image) ): ?>
+	?>
 		<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-	<?php endif; ?>
 	<div class="container flex-container">
 		<div class="banner-text">
 			<h1><?php the_field('hb_banner_title'); ?></h1>
