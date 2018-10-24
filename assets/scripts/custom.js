3ca630a06a855b5c3fb53d383130aaabc41d5c43
sagan5 <43873645+sagan5@users.noreply.github.com>
2018-10-24 15:42:17 +0300

    map fix

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -1,14 +1,14 @@
-// console.log("Pavyko!");
+console.log("Pavyko!");
 
 $(document).ready(function(){
 
-    // burger
-	$('.burger').click(function(){
-		$('.nav').toggleClass('show');
-	});
-	$('.menu-item').click(function(){
-		$('.nav').toggleClass('show');
-	});
+    // burger menu
+    $('.burger').click(function(){
+        $('.nav').toggleClass('show');
+    });
+    $('.menu-item').click(function(){
+        $('.nav').toggleClass('show');
+    });
 
     // banner carousel
     $('.slick-carousel').slick({
@@ -31,14 +31,162 @@ $(document).ready(function(){
     })
 });
 
-// google maps
-function myMap() {
-    var mapCanvas = document.getElementById("map");
-    var rikis = {lat: 54.6746045, lng: 25.2804137};
-    var mapOptions = {
-        center: new google.maps.LatLng(rikis),
-        zoom: 14
+
+
+(function($) {
+
+/*
+*  render_map
+*
+*  This function will render a Google Map onto the selected jQuery element
+*
+*  @type    function
+*  @date    8/11/2013
+*  @since   4.3.0
+*
+*  @param   $el (jQuery element)
+*  @return  n/a
+*/
+
+function render_map( $el ) {
+
+    // var
+    var $markers = $el.find('.marker');
+
+    // vars
+    var args = {
+        zoom        : 20,
+        center      : new google.maps.LatLng(0, 0),
+        mapTypeId   : google.maps.MapTypeId.ROADMAP
     };
-    var map = new google.maps.Map(mapCanvas, mapOptions);
-    var marker = new google.maps.Marker({position: rikis, map: map});
+
+    // create map               
+    var map = new google.maps.Map( $el[0], args);
+
+    // add a markers reference
+    map.markers = [];
+
+    // add markers
+    $markers.each(function(){
+
+        add_marker( $(this), map );
+
+    });
+
+    // center map
+    center_map( map );
+
 }
+
+/*
+*  add_marker
+*
+*  This function will add a marker to the selected Google Map
+*
+*  @type    function
+*  @date    8/11/2013
+*  @since   4.3.0
+*
+*  @param   $marker (jQuery element)
+*  @param   map (Google Map object)
+*  @return  n/a
+*/
+
+function add_marker( $marker, map ) {
+
+    // var
+    var latlng = new google.maps.LatLng( $marker.attr('data-lat'), $marker.attr('data-lng') );
+
+    // create marker
+    var marker = new google.maps.Marker({
+        position    : latlng,
+        map         : map
+    });
+
+    // add to array
+    map.markers.push( marker );
+
+    // if marker contains HTML, add it to an infoWindow
+    if( $marker.html() )
+    {
+        // create info window
+        var infowindow = new google.maps.InfoWindow({
+            content     : $marker.html()
+        });
+
+        // show info window when marker is clicked
+        google.maps.event.addListener(marker, 'click', function() {
+
+            infowindow.open( map, marker );
+
+        });
+    }
+
+}
+
+/*
+*  center_map
+*
+*  This function will center the map, showing all markers attached to this map
+*
+*  @type    function
+*  @date    8/11/2013
+*  @since   4.3.0
+*
+*  @param   map (Google Map object)
+*  @return  n/a
+*/
+
+function center_map( map ) {
+
+    // vars
+    var bounds = new google.maps.LatLngBounds();
+
+    // loop through all markers and create bounds
+    $.each( map.markers, function( i, marker ){
+
+        var latlng = new google.maps.LatLng( marker.position.lat(), marker.position.lng() );
+
+        bounds.extend( latlng );
+
+    });
+
+    // only 1 marker?
+    if( map.markers.length == 1 )
+    {
+        // set center of map
+        map.setCenter( bounds.getCenter() );
+        map.setZoom( 18 );
+    }
+    else
+    {
+        // fit to bounds
+        map.fitBounds( bounds );
+    }
+
+}
+
+/*
+*  document ready
+*
+*  This function will render each map when the document is ready (page has loaded)
+*
+*  @type    function
+*  @date    8/11/2013
+*  @since   5.0.0
+*
+*  @param   n/a
+*  @return  n/a
+*/
+
+$(document).ready(function(){
+
+    $('.acf-map').each(function(){
+
+        render_map( $(this) );
+
+    });
+
+});
+
+})(jQuery);
